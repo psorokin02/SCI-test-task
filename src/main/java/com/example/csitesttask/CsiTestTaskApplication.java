8e9d75f0b80e9c58646fcdeb20e4bb89f759b0f1
sorokin <sorokin@protei.ru>
2022-11-23 10:23:35 +0300

    init

---
+++ src/main/java/com/example/csitesttask/CsiTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.csitesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsiTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsiTestTaskApplication.class, args);
+    }
+
+}
